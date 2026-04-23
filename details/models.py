a8b5549fb4c5c8fbc05bdcdb253df64005c05da3
Payer Ahmed <payerahmedopu@gmail.com>
2026-04-24 03:10:51 +0600

    update code by models space

--- details/models.py
+++ details/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 from userauth.models import *
 
-
-
 # player season
 class Season(models.Model):
     player = models.ForeignKey(User,on_delete=models.CASCADE, related_name='season_model', null=True, blank=True)
     season_year = models.CharField(max_length=20,unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
 
     def __str__(self):
         return self.player.username
