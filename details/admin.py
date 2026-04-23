775e659b9eaba09640344a1d03942bde4f2d494d
Payer Ahmed <payerahmedopu@gmail.com>
2026-04-24 03:00:51 +0600

    update code fixing bug admin file write playermatch model

--- details/admin.py
+++ details/admin.py
@@ -5,5 +5,6 @@ from .models import *
 admin.site.register(Season)
 admin.site.register(Month)
 admin.site.register(PlayerWeekdetails)
+admin.site.register(PlayerMatch)
 admin.site.register(Match)
 admin.site.register(News)
